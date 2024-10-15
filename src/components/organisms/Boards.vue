f41775c39b0a2380067abf37f5575c4ed010cff0
martoms <tomataomarjohn@gmail.com>
2024-10-15 16:42:57 +0800

    add Boards, Board, and Task vue

---
+++ src/components/organisms/Boards.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+  import { toRefs, ref } from 'vue';
+
+  const draggedItem = ref('')
+  const dropZone = ref('')
+
+  const boards = [
+    {id: 1, class: 'todo', header: 'To Do'},
+    {id: 2, class: 'in-progress', header: 'In Progress'},
+    {id: 3, class: 'done', header: 'Done'}
+  ]
+
+  function updateDraggedItem(item: string) {
+    draggedItem.value = item
+  }
+
+  function updateDropZone(zone: string) {
+    dropZone.value = zone
+  }
+
+  
+</script>
+
+<template>
+  <div class="boards">
+    
+  </div>
+</template>
+
+<style scoped>
+  div.boards {
+    height: 70vh;
+    display: flex;
+    justify-content: space-evenly;
+    gap: 5px;
+  }
+</style>
